0c1cd3708358dab2d1e54a7c0b8106c59cccf916
skkwon <skkwon.partner@iloen.com>
2017-09-14 09:28:38 +0900

    MM. rx 로 변경

--- FRPTutorial/ViewController.swift
+++ FRPTutorial/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
  
-    var name: String = "Eddie"
-
+    //원래: var name: String = "Eddie"
+    
+    //수정: 아래처럼 Variable 타입으로 선언함
+    let name: Variable<String> = Variable("Eddie")
+    let disposableBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        name.asObservable()
+            .bindTo(nameLabel.rx.text)
+            .addDisposableTo(disposableBag)
+
     }
     
     @IBAction func changeNameButtonTapped(_ sender: AnyObject) {
@@ -26,7 +34,6 @@ class ViewController: UIViewController {
         showAlertController()
     
     }
-    
     
     func showAlertController() {
         let title = "Update Name"
@@ -37,15 +44,22 @@ class ViewController: UIViewController {
         
         let okAction = UIAlertAction(title: "OK", style: .default) { action in
             if let textInput = alert.textFields?.first?.text {
-                self.name = textInput
-                self.nameLabel.text = self.name
+                
+                //원래:
+                //self.name = textInput
+                //self.nameLabel.text = self.name
+                
+                
+                //수정:  입력된 텍스트로 value 속성을 바꾸자!!!
+                self.name.value = textInput
+                
             }
         }
         
         //핸들러 추가
         alert.addAction(cancelAction)
         alert.addAction(okAction)
-        alert.addTextField()  //입력창 추가
+        alert.addTextField()
         
         //화면 표시
         present(alert, animated: true)
